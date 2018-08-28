4177659bf6f1ad58d9b20987dcab649651e6c766
amitlevy21 <amitlevy347@gmail.com>
2018-08-28 07:10:07 +0300

    Documentation for io_service.py, remove legacy_reader method

--- rnnlm/models/lstm_fast/io_service.py
+++ rnnlm/models/lstm_fast/io_service.py
@@ -17,9 +17,16 @@ def raw_to_tf_records(raw_path, tf_record_path, vocab_path, seq_len, overlap=Fal
         tf_record_path: (str)
         vocab_path: (str) path of raw format vocabulary
         seq_len: (int)
-
+        overlap: (bool) whether the data should written and then read with overlaps.
+            e.g. suppose the sentence "The quick brown fox jumped over the lazy dog"
+            given seq_len=2 and say batch_size=2 we will get the following:
+            x1=[[The, quick], [quick, brown]], x2=[[brown, fox], [fox, jumped]]
+            y1=[[quick, brown], [brown, fox]], y2=[[fox, jumped], [jumped, over]]
+            notice the overlap for example the word quick that appears in x11 and x12
+            and the overlap between batches (the word brown appears in x1 and x2)
+            y is represented accordingly only shifted by 1
     Returns:
-
+        None
     """
     raw_file = tf.gfile.GFile(raw_path, "r")
     vocab_file = tf.gfile.GFile(vocab_path, "r")
