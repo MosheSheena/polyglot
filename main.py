c59b2b02d38cdfc22b68e76819d404725c862f46
amitlevy21 <amitlevy347@gmail.com>
2018-12-30 18:10:33 +0200

    wip - create a single config/hyperparameters file

--- main.py
+++ main.py
@@ -18,46 +18,46 @@ if __name__ == "__main__":
                       checkpoint_path=abs_save_path,
                       shared_hyperparams=shared_hyperparams)
 
-    tasks_hyperparams = dict()
-    for model in experiment_config.models:
-        tasks_hyperparams[model] = load_params(
-            os.path.join(os.getcwd(), "rnnlm/models/{}/hyperparameters.json".format(model))
-        )
+    for experiment in experiment_config.experiments:
+        print("running experiment {}".format(experiment.experiment_name))
+        tasks_hyperparams = dict()
+        for model in experiment.models:
+            tasks_hyperparams[model] = experiment.hyperparameters.model
 
-        # TODO - perform as many check as possible on hyperparams JSON
-        if not tasks_hyperparams[model].problem.data_path:
-            raise ValueError("Must set data_path hyperparameters.json")
+            # TODO - perform as many check as possible on hyperparams JSON
+            if not tasks_hyperparams[model].problem.data_path:
+                raise ValueError("Must set data_path hyperparameters.json")
 
-        pre_training = importlib.import_module("rnnlm.models.{}.pre_training".format(model))
-        task = importlib.import_module("rnnlm.models.{}.task".format(model))
+            pre_training = importlib.import_module("rnnlm.models.{}.pre_training".format(model))
+            task = importlib.import_module("rnnlm.models.{}.task".format(model))
 
-        if tasks_hyperparams[model].problem.convert_raw_to_tf_records:
-            print("Converting raw data to tfrecord format")
-            pre_training.main(shared_hyperparams=shared_hyperparams,
-                              hyperparams=tasks_hyperparams[model])
+            if tasks_hyperparams[model].problem.convert_raw_to_tf_records:
+                print("Converting raw data to tfrecord format")
+                pre_training.main(shared_hyperparams=shared_hyperparams,
+                                  hyperparams=tasks_hyperparams[model])
 
-        task_to_train = task.create_task(shared_hyperparams=shared_hyperparams,
-                                         hyperparams=tasks_hyperparams[model])
-        trainer.add_task(task_to_train)
+            task_to_train = task.create_task(shared_hyperparams=shared_hyperparams,
+                                             hyperparams=tasks_hyperparams[model])
+            trainer.add_task(task_to_train)
 
-    technique = experiment_config.learning_technique
-    print("Start training")
+        technique = experiment_config.learning_technique
+        print("Start training")
 
-    if technique == "normal":
-        trainer.train_normal()
-    elif technique == "transfer":
-        trainer.train_transfer_learning()
-    elif technique == "multitask":
-        switch_each_epoch = shared_hyperparams.train.switch_each_epoch
-        switch_each_batch = shared_hyperparams.train.switch_each_batch
-        num_multitask_epochs = shared_hyperparams.train.num_multitask_epochs
+        if technique == "normal":
+            trainer.train_normal()
+        elif technique == "transfer":
+            trainer.train_transfer_learning()
+        elif technique == "multitask":
+            switch_each_epoch = shared_hyperparams.train.switch_each_epoch
+            switch_each_batch = shared_hyperparams.train.switch_each_batch
+            num_multitask_epochs = shared_hyperparams.train.num_multitask_epochs
 
-        trainer.train_multitask(switch_each_epoch, switch_each_batch, num_multitask_epochs)
-    else:
-        raise NotImplemented(
-            "unsupported learning technique {}\nonly normal, transfer and multitask are supported.".format(technique)
-        )
+            trainer.train_multitask(switch_each_epoch, switch_each_batch, num_multitask_epochs)
+        else:
+            raise NotImplemented(
+                "unsupported learning technique {}\nonly normal, transfer and multitask are supported.".format(technique)
+            )
 
-    print("End training")
+        print("End training")
 
     print("end time: {}".format(datetime.now()))
