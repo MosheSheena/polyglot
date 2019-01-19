e218aa83e0a705357dad9e11dfb1a34170a815b3
moshesheena <moshesheena@gmail.com>
2019-01-19 17:29:21 +0200

    Using train tensors in loading

--- rnnlm/tools/metrics/prob.py
+++ rnnlm/tools/metrics/prob.py
@@ -16,15 +16,28 @@ from datetime import datetime
 import tensorflow as tf
 
 # Tensors required to load from model
+# TENSORS_OF_MODEL_DICT = {
+#     "word_in": "Test/Model/lstm_fast/test_word_in",
+#     "word_out": "Test/Model/lstm_fast/test_word_out",
+#     "initial_state": "Test/Model/lstm_fast/test_initial_state",
+#     "state_in": "Test/Model/lstm_fast/test_state_in",
+#     "state_out": "Test/Model/lstm_fast/test_state_out",
+#     "cell_in": "Test/Model/lstm_fast/test_cell_in",
+#     "cell_out": "Test/Model/lstm_fast/test_cell_out",
+#     "test_out": "Test/Model/lstm_fast/test_out",
+#     "softmax_w": "Model/lstm_fast/softmax_w",
+#     "softmax_b": "Model/lstm_fast/softmax_b"
+# }
+
 TENSORS_OF_MODEL_DICT = {
-    "word_in": "Test/Model/lstm_fast/test_word_in",
-    "word_out": "Test/Model/lstm_fast/test_word_out",
-    "initial_state": "Test/Model/lstm_fast/test_initial_state",
-    "state_in": "Test/Model/lstm_fast/test_state_in",
-    "state_out": "Test/Model/lstm_fast/test_state_out",
-    "cell_in": "Test/Model/lstm_fast/test_cell_in",
-    "cell_out": "Test/Model/lstm_fast/test_cell_out",
-    "test_out": "Test/Model/lstm_fast/test_out",
+    "word_in": "Train/Model/lstm_fast/test_word_in",
+    "word_out": "Train/Model/lstm_fast/test_word_out",
+    "initial_state": "Train/Model/lstm_fast/test_initial_state",
+    "state_in": "Train/Model/lstm_fast/test_state_in",
+    "state_out": "Train/Model/lstm_fast/test_state_out",
+    "cell_in": "Train/Model/lstm_fast/test_cell_in",
+    "cell_out": "Train/Model/lstm_fast/test_cell_out",
+    "test_out": "Train/Model/lstm_fast/test_out",
     "softmax_w": "Model/lstm_fast/softmax_w",
     "softmax_b": "Model/lstm_fast/softmax_b"
 }
