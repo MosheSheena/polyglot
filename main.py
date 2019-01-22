e39c89b45ba59025f8223ebbc17d2171a8b40bb8
amitlevy21 <amitlevy347@gmail.com>
2019-01-22 21:43:04 +0200

    load params at main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 from datetime import datetime
-from rnnlm.classes.experiment_runner import ExperimentRunner
+from rnnlm.classes.experiments_runner import ExperimentsRunner
+from rnnlm.utils.hyperparams import load_params
 
 
 if __name__ == "__main__":
     print("start time: {}".format(datetime.now()))
 
-    config = os.path.join(os.getcwd(), "experiment_config.json")
-    experiment_runner = ExperimentRunner(config)
+    config_file = os.path.join(os.getcwd(), "experiment_config.json")
+    config = load_params(config_file)
+
+    experiment_runner = ExperimentsRunner(config)
     experiment_runner.run()
 
     print("end time: {}".format(datetime.now()))
