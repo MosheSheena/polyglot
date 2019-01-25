e71172e0fa0213605bab0b14f91bcf0ca3ff7fc6
moshesheena <moshesheena@gmail.com>
2019-01-25 12:55:23 +0200

    Removed test scope tensors from tools

--- rnnlm/tools/generator/generator.py
+++ rnnlm/tools/generator/generator.py
@@ -40,19 +40,6 @@ WORD_BUFFER_SIZE = 50
 GENERATION_MSG = "Entered = {0} --> Generated = {1}"
 
 # Tensors required to load from model
-# TENSORS_OF_MODEL_DICT = {
-#     "word_in": "Test/Model/lstm_fast/test_word_in",
-#     "word_out": "Test/Model/lstm_fast/test_word_out",
-#     "initial_state": "Test/Model/lstm_fast/test_initial_state",
-#     "state_in": "Test/Model/lstm_fast/test_state_in",
-#     "state_out": "Test/Model/lstm_fast/test_state_out",
-#     "cell_in": "Test/Model/lstm_fast/test_cell_in",
-#     "cell_out": "Test/Model/lstm_fast/test_cell_out",
-#     "test_out": "Test/Model/lstm_fast/test_out",
-#     "softmax_w": "Model/lstm_fast/softmax_w",
-#     "softmax_b": "Model/lstm_fast/softmax_b"
-# }
-
 TENSORS_OF_MODEL_DICT = {
     "word_in": "Train/Model/lstm_fast/test_word_in",
     "word_out": "Train/Model/lstm_fast/test_word_out",
